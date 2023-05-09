301d8aa8ed4e49d88b895ab075f5d9b5f1767423
Adindu Joyce <116634790+Zhanxunmei@users.noreply.github.com>
2023-05-09 23:17:37 +0100

    Ongoing Project
    This Project is yet to be completed

---
+++ FormSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddSection = () => {
+  return (
+    <div className='Addtask'>
+        <input type='text' placeholder='Add a new task'/>
+        <button className='enter'>Add</button>
+
+    </div>
+  )
+}
+
+export default AddSection
---
+++ README.md
@@ -0,0 +1,11 @@
+# TO DO LIST
+I want to create a To do list App.
+
+# Component Breakdown
+- ToDo App
+- Add Section
+-ToDo List
+- ToDo Item
+- Buttons (edit, delete)
+
+//Yet to complete this project.
